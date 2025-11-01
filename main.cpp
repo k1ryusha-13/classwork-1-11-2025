5152887c07a2f4527d42859b3e690697b6081ed7
kirusha-13 <kirillukashevich2007@gmail.com>
2025-11-01 12:41:07 +0300

    create

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+void destroy(int ** mtx, size_t created)
+{
+  
+}
+int ** create(size_t rows, size_t cols)
+{
+  int ** mtx = new int * [rows];
+  size_t created = 0;
+  try
+  {    
+    for (; created < rows, ++created)
+    {
+      mtx[created] = new int[cols];
+    }
+  }
+  catch (const std::bad_alloc & e)
+  {
+
+  }
+  return mtx;
+}
+
 int main()
 {
-  std::cout << :Hello world!\n";
+  int ** matrix = nullptr;
+  matrix = create(5,5);
 }
